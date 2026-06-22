7731b5f663e95514799e964dee0929f6d584d2cc
leoaxo098 <leoffpro098@gmail.com>
2026-06-23 00:06:05 +0700

    fix: use Viet tierlist rank instead of points for global rank display

--- common/src/main/java/net/uku3lig/tiertagger/tierlist/VietTierListConverter.java
+++ common/src/main/java/net/uku3lig/tiertagger/tierlist/VietTierListConverter.java
@@ -34,7 +34,7 @@ public final class VietTierListConverter {
                 toRankings(response),
                 p.region(),
                 p.points(),
-                /* overall = */ p.points(),
+                /* overall = */ p.rank(),
                 /* badges   = */ Collections.emptyList(),
                 /* combatMaster = */ false
         );
