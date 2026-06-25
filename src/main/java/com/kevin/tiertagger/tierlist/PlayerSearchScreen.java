1b3686a9d4be844e9ad04a5f28b4682063f316f7
leoaxo098 <leoffpro098@gmail.com>
2026-06-25 21:28:53 +0700

    Fix NPE when Viet tierlist search returns null player info

--- src/main/java/com/kevin/tiertagger/tierlist/PlayerSearchScreen.java
+++ src/main/java/com/kevin/tiertagger/tierlist/PlayerSearchScreen.java
@@ -81,7 +81,12 @@ public class PlayerSearchScreen extends CloseableScreen {
         });
 
         this.future = TierCache.searchPlayer(username)
-                .thenCombine(skinFuture, (info, skin) -> new PlayerInfoScreen(this, info, skin))
+                .thenCombine(skinFuture, (info, skin) -> {
+                    if (info == null) {
+                        throw new NullPointerException("Player info is null");
+                    }
+                    return new PlayerInfoScreen(this, info, skin);
+                })
                 .thenAccept(screen -> MinecraftClient.getInstance().execute(() -> MinecraftClient.getInstance().setScreen(screen)))
                 .whenComplete((v, t) -> {
                     if (t != null) {
